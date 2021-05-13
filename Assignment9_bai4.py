fa4bba65992e38fae87767a249c4310405032b05
lequangnhatit2 <44295926+lequangnhatit2@users.noreply.github.com>
2021-05-13 22:08:47 +0700

    Update Assignment9_bai4.py

--- Assignment9_bai4.py
+++ Assignment9_bai4.py
@@ -8,6 +8,12 @@ driver = webdriver.Chrome(chrome_driver_path)
 
 driver.get("http://practice.automationtesting.in/")
 driver.maximize_window()
-print(driver.title)
-time.sleep(7)
-driver.close()
+try:
+	assert "Automation Practice Site" in driver.title
+	print(driver.title)
+	print('OK')
+	time.sleep(2)
+	driver.close()
+
+except Exception as e:
+	print(e)
